d5a33e20795430b36a2f355b51dac364f0096a92
Breno <b88660501@gmail.com>
2021-09-16 23:51:44 -0300

    logout

--- lib/app/movie/screens/watchlist.dart
+++ lib/app/movie/screens/watchlist.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:movie_sky/app/auth/stores/auth_store.dart';
 import 'package:movie_sky/app/movie/components/watch_list_movie.dart';
 import 'package:movie_sky/app/movie/stores/index_movies_store.dart';
 import 'package:movie_sky/app/movie/stores/watchlist_store.dart';
@@ -9,6 +10,7 @@ class WatchList extends StatelessWidget {
   WatchList({ Key? key }) : super(key: key);
   
   final watchListStore = Modular.get<WatchListStore>();
+  final authStore = Modular.get<AuthStore>();
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +18,14 @@ class WatchList extends StatelessWidget {
     watchListStore.getMovies();
 
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        onPressed: () async {
+          await authStore.logout();
+          Modular.to.pushNamed('/auth/login');
+        },
+        child: Icon(Icons.logout)
+      ),
       body: Padding(
         padding: EdgeInsets.symmetric(vertical: 24, horizontal: 12),
         child: ListView(
